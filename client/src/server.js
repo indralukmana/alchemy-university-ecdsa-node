4ec272c0b4f0791944855d612b9e66ce60ab6d08
Indra Lukmana <indralukmana@live.com>
2024-06-25 00:50:39 +0700

    build: :sparkles: adjust env variable for react app api

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+export const baseURL = import.meta.env.REACT_APP_API_URL;
 
 const server = axios.create({
-  baseURL: "http://localhost:3042",
+  baseURL,
 });
 
 export default server;
